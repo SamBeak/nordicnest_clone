ffef523a0b1ce5863cf5478f8fd03f33ec6130de
Hyungyu <whdmsspt@naver.com>
2023-06-14 16:00:56 +0900

    Feat onTopTextBar
    1. 탑 텍스트 바 기능 구현

--- src/components/wrap/OnTopComponent.jsx
+++ src/components/wrap/OnTopComponent.jsx
@@ -1,43 +1,240 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import $ from 'jquery';
 
 export default function OnTopComponent() {
-    // const [ state, setState ] = React.useState({
-    //     isPointer: false
-    // });
+
+    const [ state, setState ] = React.useState({
+        onTopText : []
+    });
+
+    React.useEffect(() => {
+        axios({
+            url: './data/onTop/onTop.json',
+            method: 'GET'
+        })
+        .then((res) => {
+            if(res.status===200){
+                setState({
+                    ...state,
+                    onTopText : res.data.onTopText
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    },[]);
+
+    React.useEffect(() => {
+        const $onTopPointer = $('#onTop .onTop__pointer:first-child');
+        const $onTopPointer2 = $('#onTop .onTop__pointer:nth-child(2)');
+        const $onTopPointer3 = $('#onTop .onTop__pointer:nth-child(3)');
+        const $onTopPointer4 = $('#onTop .onTop__pointer:last-child');
+
+        let cnt = 0;
+        let boxCnt = 0;
+        let setId = 0;
+
+        function slidePointer(){
+            if(cnt === 1){
+                if(boxCnt === 1){
+                    $onTopPointer.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(0%)",
+                        transition: "transfrom 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1",
+                        display: "block"
+                    });
+                    $onTopPointer2.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                    $onTopPointer3.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                    $onTopPointer4.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                }
+                else if(boxCnt === 2){
+                    $onTopPointer2.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(-150%)",
+                        transition: "transform 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1"
+                    });
+                }
+                else if(boxCnt === 3){
+                    $onTopPointer3.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                    $onTopPointer4.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(0%)",
+                        transition: "transfrom 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1",
+                        display: "block"
+                    });
+                }
+            }
+            else if(cnt === 2){
+                if( boxCnt === 1){
+                    $onTopPointer.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(-150%)",
+                        transition: "transform 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1"
+                    });
+                }
+                else if(boxCnt === 2){
+                    $onTopPointer2.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                    $onTopPointer3.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(0%)",
+                        transition: "transfrom 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1",
+                        display: "block"
+                    });
+                }
+                else if(boxCnt === 3){
+                    $onTopPointer4.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(-150%)",
+                        transition: "transform 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1"
+                    });
+                }
+            }
+            else if(cnt === 3){
+                if(boxCnt === 1){
+                    $onTopPointer.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                    $onTopPointer2.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(0%)",
+                        transition: "transfrom 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1",
+                        display: "block"
+                    });
+                }
+                else if(boxCnt === 2){
+                    $onTopPointer3.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(-150%)",
+                        transition: "transform 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1"
+                    });
+                }
+                else if(boxCnt === 3){
+                    $onTopPointer4.css({
+                        pointerEvents: "none",
+                        transform: "",
+                        transition: "",
+                        opacity: "",
+                        display: "none"
+                    });
+                    $onTopPointer.css({
+                        pointerEvents: "auto",
+                        transform: "translateY(0%)",
+                        transition: "transfrom 700ms ease 0s, opacity 700ms ease 0s",
+                        opacity: "1",
+                        display: "block"
+                    });
+                }
+            }
+        }
+
+        function slideCnt(){
+            if(cnt >= 3){ 
+                cnt = 0 ;
+                cnt++;
+                if(boxCnt > 3){
+                    boxCnt = 0;
+                }
+                else{
+                    boxCnt++;
+                }
+                slidePointer();
+            }
+            else{
+                cnt++;
+                slidePointer();
+            }
+        }
+
+        function autoTimer(){
+            clearInterval(setId);
+            setId = setInterval(slideCnt, 1000);
+        }
+        autoTimer();
+    });
+
   return (
     <div id="onTop">
-        <div>
-            <p></p>
-        </div>
-        <div className="onTop__center">
-            <div className="onTop__pointer">
-                <p>
-                    <span> 👍북유럽 </span>
-                    <b>최대</b>
-                    <span> 리빙 인테리어 셀렉트샵</span>
-                </p>
+        <div className="onTop__container">
+            <div>
+                <p></p>
             </div>
-            {/* <div className="onTop__pointer">
-                <p>✈ 18만원 이상 한국무료직배송</p>
+            <div className="onTop__center">
+                {
+                    state.onTopText.map((item, idx) => {
+                        if(idx === 0){
+                            return(
+                                <div key={idx} className="onTop__pointer">
+                                    <p>
+                                        <span>{item.msg}</span>
+                                        <b>{item.msg2}</b>
+                                        <span>{item.msg3}</span>
+                                    </p>
+                                </div>
+                            )
+                        }
+                        else{
+                            return(
+                                <div key={idx} className="onTop__pointer">
+                                    <p>{item.msg}</p>
+                                </div>
+                            )
+                        }
+                    })
+                }
             </div>
-            <div className="onTop__pointer">
-                <p>🎁 매일 진행되는 세일</p>
+            <div className="onTop__right">
+                <span className="onTop__lang">
+                    <div className="lang__box">
+                        <img src="./images/kor.svg" alt="한국 국기" />
+                        <span>한국어</span>
+                    </div>
+                </span>
+                <i className="fa-solid fa-chevron-down"></i>
             </div>
-            <div className="onTop__pointer">
-                <p>🛒 카드사별 무이자할부 혜택</p>
-            </div>
-            <div className="onTop__pointer">
-                <p>💻 이 사이트는 크롬 & 엣지 브라우저에 최적화되어 있습니다.</p>
-            </div> */}
-        </div>
-        <div className="onTop__right">
-            <span className="onTop__lang">
-                <div className="lang__box">
-                    <img src="./images/kor.svg" alt="한국 국기" />
-                    <span>한국어</span>
-                </div>
-            </span>
-            <i class="fa-solid fa-chevron-down"></i>
         </div>
     </div>
   )
