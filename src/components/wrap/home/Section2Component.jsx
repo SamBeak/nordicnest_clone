6fb0194f42e59a6696b480447f3bc551217110c4
Hyungyu <whdmsspt@naver.com>
2023-06-15 10:26:52 +0900

    Feat Section2
    1. 홈 => 섹션2 구현

---
+++ src/components/wrap/home/Section2Component.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Section2Component() {
+  return (
+    <section id="section2">
+        <a href="!#">
+            <div className="container">
+                <div className="gap">
+                    <div className="title">
+                        <p>
+                            <span>여름 인테리어에 필요한 모든 것</span>
+                            <br />
+                            <span>SUMMER NEST</span>
+                        </p>
+                        <div className="btn__box">
+                            <button>썸머 샵 바로가기</button>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <img src="./images/pages/section2/main_img1.jpg" alt="피크닉 사진" />
+                    </div>
+                </div>
+            </div>
+        </a>
+    </section>
+  )
+}
