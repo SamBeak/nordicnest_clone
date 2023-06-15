5127e9a8c5c34038e77460b05b197a37d26fe0b8
Hyungyu <whdmsspt@naver.com>
2023-06-15 12:08:48 +0900

    Feat Section3
    1. 홈 => 섹션3 구현

---
+++ src/components/wrap/home/Section3Component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Section3Component() {
+  return (
+    <section id="section3">
+        <div className="container">
+            <div className="gap">
+                <div className="content">
+                    <ul className="category__box">
+                        <li><a href="!#">머그컵</a></li>
+                        <li><a href="!#">유리잔</a></li>
+                        <li><a href="!#">접시</a></li>
+                        <li><a href="!#">무선 조명</a></li>
+                        <li><a href="!#">화병 & 화분</a></li>
+                        <li><a href="!#">여름 기획전 <br />자세히 보기</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
